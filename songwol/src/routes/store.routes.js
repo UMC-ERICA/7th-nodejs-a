7d7cc4222c1f50019109890f27f123827f3edf9a
KIMMINJI <amber_lucy@naver.com>
2024-11-09 18:46:45 +0900

    feat : 수정

--- songwol/src/routes/store.routes.js
+++ songwol/src/routes/store.routes.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import { addStore } from '../controllers/store.controller.js';
+import * as storeController from '../controllers/store.controller.js';
 
 const router = express.Router();
 
 // 특정 지역에 가게 추가하기
-router.post('/', addStore);
+router.post('/', storeController.addStore);
 
 // 리뷰 추가
-router.post("/add/:storeId", addReviewToStore);
+router.post("/add/:storeId", storeController.addReviewToStore);
 
 // 리뷰 조회
-router.get("/get/:userId", showReview);
+router.get("/get/:userId", storeController.showReview);
 
 export default router;

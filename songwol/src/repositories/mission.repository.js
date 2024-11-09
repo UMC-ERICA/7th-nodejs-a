5baaa73801a243ec5e0a446c74f0473b2f0eb41c
KIMMINJI <amber_lucy@naver.com>
2024-11-09 17:41:01 +0900

    fix : 불필요한 에러처리 제외

--- songwol/src/repositories/mission.repository.js
+++ songwol/src/repositories/mission.repository.js
@@ -2,118 +2,89 @@ import { prisma } from "../db.config.js";
 import { MissionStatus } from '@prisma/client';
 
 export const createMission = async (storeId, missionData) => {
-  try {
-    const mission = await prisma.mission.create({
-      data: {
-        storeId: storeId,
-        task: missionData.task,
-        savePoints: missionData.savePoints,
-        deadline: new Date(missionData.deadline),
-      },
-    });
-    return mission;
-  } catch (err) {
-    throw new Error(`미션 추가 중 오류가 발생했습니다: ${err.message}`);
-  }
+  const mission = await prisma.mission.create({
+    data: {
+      storeId: storeId,
+      task: missionData.task,
+      savePoints: missionData.savePoints,
+      deadline: new Date(missionData.deadline),
+    },
+  });
+  return mission;
 };
 
 export const findMissionById = async (missionId) => {
-  try {
-    const mission = await prisma.mission.findUnique({
-      where: { id: missionId },
-    });
-    return mission;
-  } catch (err) {
-    throw new Error(`미션 조회 중 오류가 발생했습니다: ${err.message}`);
-  }
+  const mission = await prisma.mission.findUnique({
+    where: { id: missionId },
+  });
+  return mission;
 };
 
 export const showStoreMission = async (storeId) => {
-  try {
-    const showMission = await prisma.mission.findMany({
-      where: { id: storeId },
-    });
-    return showMission;
-  } catch (error) {
-    throw new Error(`가게 미션 조회 중 오류가 발생했습니다.: ${error.message}`)
-  }
+  const showMission = await prisma.mission.findMany({
+    where: { id: storeId },
+  });
+  return showMission;
 };
 
 export const findChallengeByUserAndMission = async (userId, missionId) => {
-  try {
-    const challenge = await prisma.userMission.findFirst({
-      where: {
-        userId: userId,
-        missionId: missionId,
-      },
-    });
-    
-    return challenge !== null;
-  } catch (err) {
-    throw new Error(`도전 조회 중 오류가 발생했습니다: ${err.message}`);
-  }
+  const challenge = await prisma.userMission.findFirst({
+    where: {
+      userId: userId,
+      missionId: missionId,
+    },
+  });
+  
+  return challenge !== null;
 };
 
 
 export const createChallenge = async (userId, missionId) => {
-  try {
-    const challenge = await prisma.userMission.create({
-      data: {
-        userId: userId,
-        missionId: missionId,
-      },
-    });
-    return challenge.id;
-  } catch (err) {
-    throw new Error(`도전 생성 중 오류가 발생했습니다: ${err.message}`);
-  }
+  const challenge = await prisma.userMission.create({
+    data: {
+      userId: userId,
+      missionId: missionId,
+    },
+  });
+  return challenge.id;
 };
 
 export const showUserMission = async (userId) => {
-  try {
-    const numericUserId = parseInt(userId, 10);
-    if (isNaN(numericUserId)) {
-      throw new Error("유효한 userId가 아닙니다.");
-    }
-
-    const showMission = await prisma.mission.findMany({
-      where: {
-        userMissions: {
-          some: {
-            userId: numericUserId,
-          },
-        },
-        success: MissionStatus.IN_PROGRESS,
-      },
-    });
-    return showMission;
-  } catch (error) {
-    throw new Error(`가게 미션 조회 중 오류가 발생했습니다.: ${error.message}`)
+  const numericUserId = parseInt(userId, 10);
+  if (isNaN(numericUserId)) {
+    throw new Error("유효한 userId가 아닙니다.");
   }
+
+  const showMission = await prisma.mission.findMany({
+    where: {
+      userMissions: {
+        some: {
+          userId: numericUserId,
+        },
+      },
+      success: MissionStatus.IN_PROGRESS,
+    },
+  });
+  return showMission;
 };
 
 export const success = async(userId, missionId) => {
-  try {
-    const updatedMission = await prisma.mission.updateMany({
-      where: {
-        id: missionId,
-        userMissions: {
-          some: {
-            userId: userId,
-          },
+  const updatedMission = await prisma.mission.updateMany({
+    where: {
+      id: missionId,
+      userMissions: {
+        some: {
+          userId: userId,
         },
-        success: MissionStatus.IN_PROGRESS,
       },
-      data: {
-        success: MissionStatus.COMPLETED,
-      },
-    });
+      success: MissionStatus.IN_PROGRESS,
+    },
+    data: {
+      success: MissionStatus.COMPLETED,
+    },
+  });
 
-    if (updatedMission.count === 0) {
-      throw new Error("missionId가 유효하지 않습니다.");
-    }
-    
-  } catch (error) {
-    throw new Error(`미션 성공 변경 중 오류가 발생했습니다.: ${error.message}`)
+  if (updatedMission.count === 0) {
+    throw new Error("missionId가 유효하지 않습니다.");
   }
 }
--- songwol/src/repositories/store.repository.js
+++ songwol/src/repositories/store.repository.js
@@ -1,16 +1,38 @@
 import { prisma } from "../db.config.js";
 
 export const createStore = async (storeData) => {
-  try {
-    const newStore = await prisma.store.create({
-      data: {
-        name: storeData.name,
-        location: storeData.location,
-        phone: storeData.phone,
-      },
-    });
-    return newStore.id;
-  } catch (error) {
-    throw new Error(`가게 추가 중 오류가 발생했습니다: ${error.message}`);
-  }
+  const newStore = await prisma.store.create({
+    data: {
+      name: storeData.name,
+      location: storeData.location,
+      phone: storeData.phone,
+    },
+  });
+  return newStore.id;
+};
+
+export const addReview = async (storeId, reviewData) => {
+  const review = await prisma.review.create({
+    data: {
+      storeId: storeId,
+      userId: reviewData.userId,
+      starvariablevalues: reviewData.starvariablevalues,
+      content: reviewData.content,
+    },
+  });
+  return review.id;
+};
+
+export const findStoreById = async (storeId) => {
+  const store = await prisma.store.findUnique({
+    where: { id: storeId },
+  });
+  return store;
+};
+
+export const showReview = async (userId) => {
+  const user = await prisma.review.findMany({
+    where: { id: userId },
+  })
+  return user;
 };
